29965131955eeefcd12f304908f5ed3da4c48555
dcristinaterry <crisdc88@gmail.com>
2020-09-09 16:23:23 -0500

    add product and brand tables

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,7 @@
 var bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function (sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+  var Users = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email_address: {
       type: DataTypes.STRING,
@@ -45,13 +45,13 @@ module.exports = function (sequelize, DataTypes) {
   
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function (password) {
+  Users.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function (user) {
+  Users.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
-  return User;
+  return Users;
 }
