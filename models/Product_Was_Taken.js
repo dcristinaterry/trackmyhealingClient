65d683a028219f6ccd7db05a6726632f5e746124
almonasterio <a.monasterio.a@gmail.com>
2020-09-10 16:28:22 -0500

    tables added and gitignore fixed

--- models/Product_Was_Taken.js
+++ models/Product_Was_Taken.js
@@ -1,7 +1,7 @@
 
 module.exports=function (sequelize,DataTypes){
     var Product_Was_Taken=sequelize.define("Product_Was_Taken",{})
-
+    
     Product_Was_Taken.associate= (models)=>{
         models.Product_Was_Taken.belongsTo(models.Tracking_Result);
         models.Product_Was_Taken.belongsTo(models.User_Product_Tracking);
